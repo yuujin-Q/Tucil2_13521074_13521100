bcef665f31209dccd11707356f5a996975b2f2ab
yuujin-Q <92687071+Eugene316@users.noreply.github.com>
2023-02-26 23:10:35 +0700

    fix: remove global

--- src/point_set.py
+++ src/point_set.py
@@ -1,25 +1,23 @@
 """
 :file: point_set.py
 
-set of points and functions used in the closest pair problem
+set of points functions used in the closest pair problem
 """
 
 import random
 
-# point set variable
-global p_set
-p_set = []
-
-def add_point(point):
+def add_point(p_set, point):
     """add new point to p_set
 
+    :param p_set: set of point(s)
     :param point: tuple of numbers
     """
     p_set.append(point)
 
-def add_rand_point(min_max_val, fprecision, dimension = 3):
+def add_rand_point(p_set, min_max_val, fprecision, dimension=3):
     """add unique random point to p_set
 
+    :param p_set: set of point(s)
     :param min_max_val: determines the minimum and maximum value for randomization
     :param fprecision: determines digit count of fractional part
     :param dimension: dimension of point to be added, default = 3
@@ -29,24 +27,26 @@ def add_rand_point(min_max_val, fprecision, dimension = 3):
         point.append(round(random.uniform(-min_max_val, min_max_val), fprecision))
     
     if tuple(point) in p_set:       # ensure p_set element uniqueness
-        add_rand_point(min_max_val, fprecision, dimension)
+        add_rand_point(p_set, min_max_val, fprecision, dimension)
     else:
-        add_point(tuple(point))
+        add_point(p_set, tuple(point))
 
-def add_n_rand_point(n, min_max_val, fprecision, dimension = 3):
+def add_n_rand_point(p_set, n, min_max_val, fprecision, dimension=3):
     """add n amount of randomized points to p_set
 
+    :param p_set: set of point(s)
     :param n: amount of points to be added
     :param min_max_val: determines the minimum and maximum value for randomization
     :param fprecision: determines digit count of fractional part
     :param dimension: dimension of point to be added, default = 3
     """
     for i in range(n):
-        add_rand_point(min_max_val, fprecision, dimension)
+        add_rand_point(p_set, min_max_val, fprecision, dimension)
 
-def get_info_string():
+def get_info_string(p_set):
     """get information about current point set
 
+    :param p_set: set of point(s)
     :return: string of current point set information
     """
     info = ""
@@ -60,7 +60,10 @@ def get_info_string():
             info += " " + str(points) + "\n"
     return info
 
-def print_info():
-    """prints information of current point set"""
-    info = get_info_string()
+def print_info(p_set):
+    """prints information of current point set
+    
+    :param p_set: set of point(s)
+    """
+    info = get_info_string(p_set)
     print(info)
